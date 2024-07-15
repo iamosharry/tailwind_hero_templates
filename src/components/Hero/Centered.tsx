6d1e590fc581726c479fd369d80063da909d5b0f
Olajide Samuel <iamosharry@gmail.com>
2024-07-15 14:17:58 +0100

    revision

--- src/components/Hero/Centered.tsx
+++ src/components/Hero/Centered.tsx
@@ -1,6 +1,9 @@
 import NavBar from "./NavBar";
 import Navtype from "../..";
 import HeroInfo from "./HeroInfo";
+import Beams from "../darkGridHero/Beams";
+// import GradientGrid from "../darkGridHero/GradientGrid";
+
 interface Props {
   navList: Navtype[];
   onclick: () => void;
@@ -12,6 +15,7 @@ const Centered = ({ navList, onclick, nav }: Props) => {
       <div className="relative pb-36 pt-2 h-[100vh] md:h-full">
         <NavBar navList={navList} onclick={onclick} nav={nav} />
         <HeroInfo />
+        <Beams />
       </div>
     </>
   );
--- src/components/Hero/HeroInfo.tsx
+++ src/components/Hero/HeroInfo.tsx
@@ -1,11 +1,14 @@
 import { LuArrowRight } from "react-icons/lu";
+import GradientGrid from "../darkGridHero/GradientGrid";
 
 const HeroInfo = () => {
   return (
     <>
-      <div className="absolute w-full h-full bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] top-0 opacity-10 z-[-1]"></div>
-      <div className="mt-20 ">
-        <div className="   mb-5 ">
+      <div className="absolute w-full h-full  to-[#9089fc] top-0 opacity-10 z-[-1] ">
+        <GradientGrid />
+      </div>
+      <div className="mt-20 z-50">
+        <div className="  mb-5 ">
           <p className="border border-gray-300 max-w-fit m-auto py-2 px-4 rounded-full text-[14px] md:text-[16px] text-[#111827] text-center">
             Announcing our next round of funding.
             <a
@@ -17,7 +20,7 @@ const HeroInfo = () => {
             </a>
           </p>
         </div>
-        <h1 className="text-[#111827] text-center text-[40px] md:text-[50px] font-bold lg:text-[65px] lg:font-bold mb-5 sm:mb-5  px-3 sm:w-[700px] sm:m-auto sm:leading-[1.2em]">
+        <h1 className=" text-[#111827] text-center text-[40px] md:text-[50px] font-bold lg:text-[65px] lg:font-bold mb-5 sm:mb-5  px-3 sm:w-[700px] sm:m-auto sm:leading-[1.2em]">
           Data to enrich your online business
         </h1>
         <p className="text-center text-gray-600 mb-8 font-poppins px-4 md:max-w-[690px] md:m-auto md:mb-7 md:font-medium md:text-[20px] ">
